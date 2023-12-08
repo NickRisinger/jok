cb4be8a89128ae2c2e76f76857e935c64d599d7b
Boozy044 <boozy044@gmail.com>
2023-12-08 19:28:54 +0300

    chore: init auth and not-found page

--- apps/crm-react/src/App.tsx
+++ apps/crm-react/src/App.tsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from './layouts/Layout';
+import IndexPage from './pages/IndexPage';
+import AuthPage from './pages/AuthPage';
+import NotFound from './pages/NotFound';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index element={<div>Index Page</div>} />
+        <Route index element={<IndexPage />} />
       </Route>
-      <Route path="/auth" element={<div>Auth Page</div>}></Route>
-      <Route path="*" element={<div>Not Found</div>}></Route>
+      <Route path="/auth" element={<AuthPage />}></Route>
+      <Route path="*" element={<NotFound />}></Route>
     </Routes>
   );
 }
