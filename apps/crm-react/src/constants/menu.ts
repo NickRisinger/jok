d102057ab494fa27ab52429b772430020922da39
Boozy044 <boozy044@gmail.com>
2023-12-08 19:25:32 +0300

    chore: add layout, routes and menu

---
+++ apps/crm-react/src/constants/menu.ts
@@ -0,0 +1,201 @@
+export const MenuLinks = [
+  {
+    icon: '',
+    label: 'Объекты',
+    link: '/objects',
+  },
+  {
+    icon: '',
+    label: 'Модерация объекта',
+    link: '/objects-moderation',
+  },
+  {
+    icon: '',
+    label: 'Парсер',
+    link: '/parser',
+  },
+  {
+    icon: '',
+    label: 'Заявки на покупку',
+    link: '/bids',
+  },
+  {
+    icon: '',
+    label: 'Сделки',
+    link: '/deals',
+  },
+  {
+    icon: '',
+    label: 'Ипотечный калькулятор',
+    link: '/mortgage-calc',
+  },
+  {
+    icon: '',
+    label: 'Заявки на оценку',
+    link: '/estimations',
+  },
+  {
+    icon: '',
+    label: 'Страхование',
+    link: '/policy',
+  },
+  {
+    icon: '',
+    label: 'Услуги',
+    link: '/services',
+  },
+  {
+    icon: '',
+    label: 'Задачи',
+    link: '/tasks',
+  },
+  {
+    icon: '',
+    label: 'Клиенты',
+    link: '/tasks',
+  },
+  {
+    icon: '',
+    label: 'Звонки',
+    link: '/calls',
+  },
+  {
+    icon: '',
+    label: 'Сотрудники',
+    link: '/users',
+  },
+  {
+    icon: '',
+    label: 'Аналитика',
+    link: '/analytics',
+  },
+  {
+    icon: '',
+    label: 'Отчёты',
+    link: '/report',
+  },
+  {
+    icon: '',
+    label: 'Архивы',
+    link: '/',
+    children: [
+      {
+        icon: '',
+        label: 'Обьекты',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Заявки на покупку',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Заявки на ипотеку',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Сделки',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Задачи',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Контрагенты',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Пользователи',
+        link: '/',
+      },
+    ],
+  },
+  {
+    icon: '',
+    label: 'Настройки',
+    link: '/',
+    children: [
+      {
+        icon: '',
+        label: 'Агентства',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Платформы и выгрузки',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Тип контрагента',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Страховые агенты',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Тип задач',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Группы',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Типы расчета',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Типы устройств',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Устройства',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Этапы сделки',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Источники',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Банки',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Оценочные компании',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Типы услуг',
+        link: '/',
+      },
+      {
+        icon: '',
+        label: 'Ставки по умолчанию',
+        link: '',
+      },
+    ],
+  },
+];
